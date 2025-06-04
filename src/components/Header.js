243d9f49a047eb0ca4ef0cf0a82217eba11ed4ea
An Hee Jae <gmlwo000810@gmail.com>
2025-06-04 20:07:19 +0900

    2025 업데이트

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header({ mat }) {
         <div className='logo'>
           <Link to={"/"}><img src={tapLogo} alt="tap 로고" /></Link>
         </div>
-        <p className='title'>2024 제11회 전국대학생주짓수대회</p>
+        <p className='title'>2025 제12회 전국대학생주짓수대회</p>
         <p className='mat'>{mat}</p>
       </div>
     </>
